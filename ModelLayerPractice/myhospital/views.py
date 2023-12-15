48d7c8d2a00f13d1daa31d7041dfe68a903a8cf0
Minh Haong <HoangAkyc29>
2023-12-15 10:52:28 +0700

    Update Custom Hospital Management Django App

--- ModelLayerPractice/myhospital/views.py
+++ ModelLayerPractice/myhospital/views.py
@@ -1,84 +1,161 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.views.generic import ListView, DetailView
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+import django.contrib.auth.models
+from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from django.http import HttpResponse, JsonResponse, Http404
+from rest_framework import viewsets
+from rest_framework import permissions
+from .permissions import IsOwnerOrReadOnly
+from rest_framework import renderers
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Doctor, Patient, Appointment, PrescriptionLines
+from .serializers import DoctorSerializer, PatientSerializer, AppointmentSerializer, PrescriptionLinesSerializer, UserSerializer, UserLoginSerializer
+from .models import MyUser
 
 
 
-class DoctorListView(ListView):
-    model = Doctor
-    template_name = 'hospital/doctor_list.html'
-    context_object_name = 'doctor_list'
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
 
-    def get_queryset1(self):
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = MyUser.objects.all()
+    serializer_class = UserSerializer
+
+
+class LoginView(APIView):
+    authentication_classes = []
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(
+                request,
+                username=serializer.validated_data["username"],
+                password=serializer.validated_data["password"],
+            )
+            if user:
+                token, created = Token.objects.get_or_create(user=user)
+                # refresh = TokenObtainPairSerializer.get_token(user)
+                data = {"access_token": str(token), "user_id": str(user.id)}
+                return Response(data, status=status.HTTP_200_OK)
+
+        return Response(
+            {"error_message": "Email or password is incorrect!", "error_code": 400},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class RegisterView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            # serializer.validated_data("password")
+            user = serializer.save()
+            # user= MyUser.objects.create_user(email=request.data.username, password=request.data.password)
+            # user.save();
+            # return Response(request.data, status=status.HTTP_201_CREATED)
+            return JsonResponse(
+                {"message": "Register successful!"}, status=status.HTTP_201_CREATED
+            )
+        else:
+            return JsonResponse(
+                {
+                    "error_message": "This email has already exist!",
+                    "errors_code": 400,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+class DoctorListView(ListAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+    def get_queryset(self):
         search_query = self.request.GET.get('search')
         if search_query:
             return Doctor.objects.filter(name__icontains=search_query)
         return Doctor.objects.all()
 
-class DoctorDetailView(DetailView):
-    model = Doctor
-    template_name = 'hospital/doctor_detail.html'
+class DoctorDetailView(RetrieveAPIView):
+    permissions_classes = [permissions.IsAuthenticated]
 
-class PatientListView(ListView):
-    model = Patient
-    template_name = 'hospital/Patient_list.html'
-    context_object_name = 'patient_list'
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
 
-    def get_queryset1(self):
+class PatientListView(ListAPIView):
+    permissions_classes = [permissions.IsAuthenticated]
+
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+    def get_queryset(self):
         search_query = self.request.GET.get('search')
         if search_query:
             return Patient.objects.filter(name__icontains=search_query)
         return Patient.objects.all()
 
-class PatientDetailView(DetailView):
-    model = Patient
-    template_name = 'hospital/patient_detail.html'
+class PatientDetailView(RetrieveAPIView):
+    permissions_classes = [permissions.IsAuthenticated]
 
-class AppointmentListView(ListView):
-    model = Appointment
-    template_name = 'hospital/Appointment_list.html'
-    context_object_name = 'appointment_list'
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
 
-    def get_queryset1(self):
+class AppointmentListView(ListAPIView):
+    permissions_classes = [permissions.IsAuthenticated]
+
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+    def get_queryset(self):
         search_query = self.request.GET.get('search')
         if search_query:
             return Appointment.objects.filter(name__icontains=search_query)
         return Appointment.objects.all()
 
-class AppointmentPatientView(ListView):
-    model = Appointment
-    template_name = 'hospital/appointment_patient_list.html'  # Đặt tên template của bạn
-    context_object_name = 'appointment_list'
+class AppointmentPatientView(ListAPIView):
+    permissions_classes = [permissions.IsAuthenticated]
+
+    serializer_class = AppointmentSerializer
 
     def get_queryset(self):
-        patient_id = self.kwargs.get('patient_id')  # Lấy ID của bệnh nhân từ đường dẫn
+        patient_id = self.kwargs.get('patient_id')
         return Appointment.objects.filter(patient__id=patient_id)
 
-class AppointmentDoctorView(ListView):
-    model = Appointment
-    template_name = 'hospital/appointment_doctor_list.html'  # Đặt tên template của bạn
-    context_object_name = 'appointment_list'
+class AppointmentDoctorView(ListAPIView):
+    permissions_classes = [permissions.IsAuthenticated]
+    serializer_class = AppointmentSerializer
 
     def get_queryset(self):
-        doctor_id = self.kwargs.get('doctor_id')  # Lấy ID của bác sĩ từ đường dẫn
+        doctor_id = self.kwargs.get('doctor_id')
         return Appointment.objects.filter(doctor__id=doctor_id)
 
-class AppointmentDetailView(DetailView):
-    model = Appointment
-    template_name = 'hospital/appointment_detail.html'  # Đặt tên template của bạn
-    context_object_name = 'appointment'
+class AppointmentDetailView(RetrieveAPIView):
+    permissions_classes = [permissions.IsAuthenticated]
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        appointment = self.object  # Lấy thông tin của appointment từ đối tượng được trả về bởi DetailView
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
 
         # Lấy danh sách PrescriptionLines tương ứng với appointment
-        prescription_lines = appointment.prescriptionlines_set.all()
+        prescription_lines = PrescriptionLines.objects.filter(appointment=instance)
+        prescription_lines_serializer = PrescriptionLinesSerializer(prescription_lines, many=True)
 
-        # Thêm danh sách PrescriptionLines vào context để hiển thị trong template
-        context['prescription_lines'] = prescription_lines
-        return context
+        # Thêm danh sách PrescriptionLines vào dữ liệu serialized để trả về
+        data = serializer.data
+        data['prescription_lines'] = prescription_lines_serializer.data
 
-
+        return Response(data)
